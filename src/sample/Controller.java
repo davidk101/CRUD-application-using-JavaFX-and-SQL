662587bb69171489abb24a7d52190f6d9551d037
David Kumar <david.dn.kumar@gmail.com>
2020-11-12 18:08:44 -0600

    establish connection

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,9 +1,29 @@
 package sample;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class Controller {
+
+    public TextField id_text;
+    public TextField year_text;
+    public TextField make_text;
+    public TextField model_text;
+    public TableView main_table;
+    public TableColumn id_column;
+    public TableColumn year_column;
+    public TableColumn make_column;
+    public TableColumn model_column;
+    public Button create_btn;
+    public Button update_btn;
+    public Button delete_btn;
 
     public Connection getConnection(){
 
@@ -18,6 +38,19 @@ public class Controller {
             System.out.println("Error:" + e.getMessage());
             return null;
         }
+    }
 
+    public ObservableList<Vehicle> getVehicles(){
+
+        ObservableList<Vehicle> Vehicles = FXCollections.observableArrayList();
+        Connection connect = getConnection();
+        String sql_query = "SELECT * FROM vehicle";
+
+        try{
+
+        }
+        catch{
+
+        }
     }
 }
